e1fcc50addfffae4381781f8b8e737a4e174ccfa
Ruham Leal <ruhamxlpro@hotmail.com>
2023-02-02 18:26:23 -0300

    test: adding test to endpoint @Get /teams/:id

--- app/backend/src/tests/team.test.ts
+++ app/backend/src/tests/team.test.ts
@@ -31,3 +31,48 @@ describe('Teste de integração para o endpoint @Get /teams', async () => {
     });
   });
 });
+
+describe('Teste de integração para o endpoint @Get /teams/:id', async () => {
+
+  afterEach(sinon.restore)
+
+  let chaiHttpResponse: Response;
+
+  describe('Testa se funciona corretamente o endpoint @Get /teams/:id', async () => {
+    it('Testa se retorna o time com o id correto', async () => {
+      sinon.stub(Team, "findOne").resolves(teams[0] as Team);
+
+      chaiHttpResponse = await chai
+        .request(app)
+        .get('/teams/1')
+
+      expect(chaiHttpResponse.status).to.be.equal(200);
+      expect(chaiHttpResponse.body).to.be.deep.equal(teams[0]);
+    });
+  });
+
+  describe('Testa se da erro ao chamar o endpoint @Get /teams/:id incorretamente', async () => {
+    it('Testa se retorna um erro quando não há time com o id passado', async () => {
+      sinon.stub(Team, "findOne").resolves(null);
+
+      chaiHttpResponse = await chai
+        .request(app)
+        .get('/teams/9999')
+        .send({ params: { id: 99999 } })
+
+      expect(chaiHttpResponse.status).to.be.equal(404);
+      expect(chaiHttpResponse.body).to.be.deep.equal({ message: 'Team not found' });
+    });
+
+    it('Testa se retorna um erro quando é passado id invalido', async () => {
+      sinon.stub(Team, "findOne").resolves(null);
+
+      chaiHttpResponse = await chai
+        .request(app)
+        .get('/teams/invalidId')
+      // console.log(chaiHttpResponse)
+      expect(chaiHttpResponse.status).to.be.equal(400);
+      expect(chaiHttpResponse.body).to.be.deep.equal({ message: 'Id must be a number' });
+    });
+  });
+});
