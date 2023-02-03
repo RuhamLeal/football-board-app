effadea04b60327f8db429efefd91222860fad02
Ruham Leal <ruhamxlpro@hotmail.com>
2023-02-02 22:35:03 -0300

    test: adding test to endpoint @get /matches/inProgress?:boolean

--- app/backend/src/tests/match.test.ts
+++ app/backend/src/tests/match.test.ts
@@ -6,7 +6,7 @@ import chaiHttp = require('chai-http');
 import { App } from '../app';
 import Match from '../database/models/Match';
 import { Response } from 'superagent';
-import { matches } from './mocks/matchMocks'
+import { matches, matchesFinished, matchesProgress } from './mocks/matchMocks'
 
 const { app } = new App();
 const { expect } = chai;
@@ -30,6 +30,51 @@ describe('Teste de integração para o endpoint @Get /matches', async () => {
 
       expect(chaiHttpResponse.status).to.be.equal(200);
       expect(chaiHttpResponse.body).to.be.deep.equal(matches);
+    });
+  });
+});
+
+describe('Teste de integração para o endpoint @Get /matches/inProgess?:boolean', async () => {
+
+  afterEach(sinon.restore)
+
+  let chaiHttpResponse: Response;
+
+  describe('Testa o funcionamento correto do endpoint @Get /matches/inProgess?:boolean', async () => {
+    it('Testa se retorna as partidas em progresso corretamente', async () => {
+      sinon.stub(Match, "findAll").resolves(matchesProgress as unknown as Match[]);
+
+      chaiHttpResponse = await chai
+        .request(app)
+        .get('/matches?inProgress=true')
+
+      expect(chaiHttpResponse.status).to.be.equal(200);
+      expect(chaiHttpResponse.body).to.be.deep.equal(matchesProgress);
+    });
+
+    it('Testa se retorna as partidas finalizadas corretamente', async () => {
+      sinon.stub(Match, "findAll").resolves(matchesFinished as unknown as Match[]);
+
+      chaiHttpResponse = await chai
+        .request(app)
+        .get('/matches?inProgress=false')
+
+      expect(chaiHttpResponse.status).to.be.equal(200);
+      expect(chaiHttpResponse.body).to.be.deep.equal(matchesFinished);
+    });
+  });
+
+  describe('Testa se da erro ao chamar o endpoint @Get /matches/inProgess?:boolean incorretamente', async () => {
+    it('Testa se retorna um erro quando nao for passado uma string booleand ("true", "false")', async () => {
+
+      chaiHttpResponse = await chai
+        .request(app)
+        .get('/matches?inProgress=invalidValue')
+
+      // console.log(chaiHttpResponse);
+
+      expect(chaiHttpResponse.status).to.be.equal(400);
+      expect(chaiHttpResponse.body).to.be.deep.equal({ message: 'Value must be "true" or "false"' });
     });
   });
 });
